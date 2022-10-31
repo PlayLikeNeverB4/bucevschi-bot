4073cffff173a6d6807e5ee177884049a2e66c1b
George Marcus <george.marcus922@gmail.com>
2022-10-31 13:05:09 +0200

    Adjust welcome message

--- app/message_handlers/greeting_handler.js
+++ app/message_handlers/greeting_handler.js
@@ -14,7 +14,8 @@ class greetingHandler extends basicResponseHandler {
     return 'Salut! Eu sunt un bot care te anunta si iti aminteste ' +
            'despre concursuri. Daca te abonezi la mine vei primi ' +
            'notificari cu o zi si cu 2 ore inainte de concursuri. ' +
-           'Foloseste optiunile din meniu.';
+           'Foloseste optiunile din meniu sau scrie manual una dintre cuvintele ' +
+           'Urmatoarele / Abonare / Dezabonare';
   }
 }
 
